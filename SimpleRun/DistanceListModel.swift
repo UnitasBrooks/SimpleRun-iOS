2eb6665b45b6dc408ac1f9a0a6bc25fdf64a0d46
Joe Yuhas <joeyuhas@joeyuhas-mac.elemental.local>
2015-05-27 13:49:48 -0700

    Create a distance list model to handle distance calculations

---
+++ SimpleRun/DistanceListModel.swift
@@ -0,0 +1,19 @@
+//
+//  DistanceListModel.swift
+//  SimpleRun
+//
+//  Created by Joe Yuhas on 5/27/15.
+//  Copyright (c) 2015 Joe Yuhas. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class DistanceList {
+    
+    private var lastLocation = CLLocation
+    
+    init() {
+        
+    }
+}
